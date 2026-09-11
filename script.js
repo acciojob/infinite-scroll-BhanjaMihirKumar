d955fb325d5004782f6e77579c9cd786b5399ae2
Mihir kumar Bhanja <139785978+BhanjaMihirKumar@users.noreply.github.com>
2026-09-11 19:16:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,21 @@
-//your code here!
 
+const list = document.getElementById("infi-list");
+
+let itemCount = 0;
+function addItems(count) {
+    for (let i = 0; i < count; i++) {
+        itemCount++;
+
+        const li = document.createElement("li");
+        li.textContent = "Item " + itemCount;
+
+        list.appendChild(li);
+    }
+}
+
+addItems(10);
+list.addEventListener("scroll", function () {
+    if (list.scrollTop + list.clientHeight >= list.scrollHeight - 5) {
+        addItems(2);
+    }
+});
